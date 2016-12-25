2cee485f38ba4ec733500d9cd86f68617e29f606
Nadjib Amar <hello@ebabel.eu>
2016-12-25 19:59:29 +0100

    todo tasks

--- build/bundle.js
+++ build/bundle.js
@@ -20793,9 +20793,13 @@
 	  // todo: mobs not spawning on top of each other (world with available or taken coordinates?).
 	  // todo: mobs moving around (no collision).
 	  // todo: mobs procreate when they are close, the female mob becomes pregnant and eventually gives birth to a number of youngs.
+	  // todo: gzip the bundle with https://www.npmjs.com/package/compression-webpack-plugin
 	  // todo: responsive canvas (not using css though).
 	  // todo: how do I navigate a world that is bigger than the viewport?
 	  // todo: corpse decay; after a while, the corpses disappear off the game (including from the corpse array).
+	  // todo: log; older log messages should be removed from the web UI to save memory. Possibly archive them in localStorage but never delete data from everywhere.
+	  // todo: use localStorage to get some persistence locally in the browser and load previous data.
+	  // todo: use online data store like https://firebase.google.com/ to store data.
 	
 	  corpses.map(function (corpse) {
 	    return drawDisc({
