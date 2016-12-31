b928404a127d94b430ca95cb22bba04bd1ea44b7
Nadjib Amar <hello@ebabel.eu>
2016-12-31 11:23:19 +0100

    button style fixed for iOS browsers

--- build/bundle.js
+++ build/bundle.js
@@ -19773,7 +19773,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _popMobs = __webpack_require__(161);
+	var _popMobs = __webpack_require__(162);
 	
 	var _ageMobs = __webpack_require__(171);
 	
@@ -19781,13 +19781,13 @@
 	
 	var _updateCanvas = __webpack_require__(173);
 	
-	var _pickMobsNextTile = __webpack_require__(180);
+	var _pickMobsNextTile = __webpack_require__(179);
 	
-	var _world = __webpack_require__(181);
+	var _world = __webpack_require__(180);
 	
 	var _world2 = _interopRequireDefault(_world);
 	
-	__webpack_require__(183);
+	__webpack_require__(182);
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
@@ -20222,6 +20222,20 @@
 
 /***/ },
 /* 161 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	// Return a formatted date and time string for the moment it is called. 
+	var now = exports.now = function now() {
+	  return new Date().toLocaleString('en-GB');
+	};
+
+/***/ },
+/* 162 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -20235,7 +20249,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _orc = __webpack_require__(162);
+	var _orc = __webpack_require__(163);
 	
 	var _orc2 = _interopRequireDefault(_orc);
 	
@@ -20306,7 +20320,7 @@
 	};
 
 /***/ },
-/* 162 */
+/* 163 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -20317,7 +20331,7 @@
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
-	var _mob = __webpack_require__(163);
+	var _mob = __webpack_require__(164);
 	
 	var _mob2 = _interopRequireDefault(_mob);
 	
@@ -20356,7 +20370,7 @@
 	exports.default = Orc;
 
 /***/ },
-/* 163 */
+/* 164 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -20371,7 +20385,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _baseClass = __webpack_require__(164);
+	var _baseClass = __webpack_require__(165);
 	
 	var _baseClass2 = _interopRequireDefault(_baseClass);
 	
@@ -20633,7 +20647,7 @@
 	exports.default = Mob;
 
 /***/ },
-/* 164 */
+/* 165 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -20642,9 +20656,9 @@
 	  value: true
 	});
 	
-	var _guid = __webpack_require__(165);
+	var _guid = __webpack_require__(166);
 	
-	var _now = __webpack_require__(166);
+	var _now = __webpack_require__(161);
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
@@ -20661,7 +20675,7 @@
 	exports.default = BaseClass;
 
 /***/ },
-/* 165 */
+/* 166 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -20676,20 +20690,6 @@
 	// Return a global unique id.
 	var guid = exports.guid = function guid() {
 	  return "" + s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
-	};
-
-/***/ },
-/* 166 */
-/***/ function(module, exports) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	// Return a formatted date and time string for the moment it is called. 
-	var now = exports.now = function now() {
-	  return new Date().toLocaleString('en-GB');
 	};
 
 /***/ },
@@ -20708,7 +20708,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _mob = __webpack_require__(163);
+	var _mob = __webpack_require__(164);
 	
 	var _mob2 = _interopRequireDefault(_mob);
 	
@@ -20789,7 +20789,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _mob = __webpack_require__(163);
+	var _mob = __webpack_require__(164);
 	
 	var _mob2 = _interopRequireDefault(_mob);
 	
@@ -20890,7 +20890,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _mob = __webpack_require__(163);
+	var _mob = __webpack_require__(164);
 	
 	var _mob2 = _interopRequireDefault(_mob);
 	
@@ -20971,7 +20971,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _mob = __webpack_require__(163);
+	var _mob = __webpack_require__(164);
 	
 	var _mob2 = _interopRequireDefault(_mob);
 	
@@ -21071,7 +21071,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _now = __webpack_require__(166);
+	var _now = __webpack_require__(161);
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
@@ -21140,9 +21140,9 @@
 	
 	var _writeCoordinates = __webpack_require__(178);
 	
-	var _animateMobMovement = __webpack_require__(179);
+	var _animateMobMovement = __webpack_require__(186);
 	
-	var _pickMobsNextTile = __webpack_require__(180);
+	var _pickMobsNextTile = __webpack_require__(179);
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
@@ -21351,50 +21351,6 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	var animateMobMovement = exports.animateMobMovement = function animateMobMovement(mob) {
-	  // Make position and destination comparable.
-	  var posY = Math.floor(mob.position.y);
-	  var posX = Math.floor(mob.position.x);
-	  var desY = Math.floor(mob.destination.y);
-	  var desX = Math.floor(mob.destination.x);
-	
-	  // Has mob arrived at destination?
-	  if (posY === desY && posX === desX) {
-	    mob.position.y = mob.destination.y;
-	    mob.position.x = mob.destination.x;
-	    mob.position.coordinateY = mob.destination.coordinateY;
-	    mob.position.coordinateX = mob.destination.coordinateX;
-	    mob.arrivedAtDestination = true;
-	
-	    return mob;
-	  }
-	
-	  // Animate movement.
-	  if (posY > desY) {
-	    mob.position.y = mob.position.y - mob.speed;
-	  }
-	  if (posY < desY) {
-	    mob.position.y = mob.position.y + mob.speed;
-	  }
-	  if (posX > desX) {
-	    mob.position.x = mob.position.x - mob.speed;
-	  }
-	  if (posX < desX) {
-	    mob.position.x = mob.position.x + mob.speed;
-	  }
-	
-	  return mob;
-	};
-
-/***/ },
-/* 180 */
-/***/ function(module, exports) {
-
-	"use strict";
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
 	// Try to pick free tiles where the mobs will move to.
 	var pickMobsNextTile = exports.pickMobsNextTile = function pickMobsNextTile(mobs, world) {
 	  mobs = mobs.map(function (mob) {
@@ -21432,7 +21388,7 @@
 	};
 
 /***/ },
-/* 181 */
+/* 180 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -21447,11 +21403,11 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _baseClass = __webpack_require__(164);
+	var _baseClass = __webpack_require__(165);
 	
 	var _baseClass2 = _interopRequireDefault(_baseClass);
 	
-	var _tile = __webpack_require__(182);
+	var _tile = __webpack_require__(181);
 	
 	var _tile2 = _interopRequireDefault(_tile);
 	
@@ -21517,7 +21473,7 @@
 	exports.default = World;
 
 /***/ },
-/* 182 */
+/* 181 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -21530,7 +21486,7 @@
 	
 	var C = _interopRequireWildcard(_constants);
 	
-	var _baseClass = __webpack_require__(164);
+	var _baseClass = __webpack_require__(165);
 	
 	var _baseClass2 = _interopRequireDefault(_baseClass);
 	
@@ -21568,16 +21524,16 @@
 	exports.default = Tile;
 
 /***/ },
-/* 183 */
+/* 182 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 	
 	// load the styles
-	var content = __webpack_require__(184);
+	var content = __webpack_require__(183);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(186)(content, {});
+	var update = __webpack_require__(185)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -21594,21 +21550,21 @@
 	}
 
 /***/ },
-/* 184 */
+/* 183 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(185)();
+	exports = module.exports = __webpack_require__(184)();
 	// imports
 	
 	
 	// module
-	exports.push([module.id, "body {\n  font-family: 'Handlee', cursive;\n  font-size: 16px;\n  font-weight: 400;\n  line-height: 1.5em;\n  margin: 0;\n  background-color: #F9F7ED;\n  color: #33170D; }\n\ninput,\ntextarea,\nkeygen,\nselect,\nbutton {\n  font-family: 'Handlee', cursive;\n  height: 32px;\n  padding: 0 6px;\n  border: 0; }\n\nh1 {\n  margin: 0 6px 0 0;\n  line-height: 49px;\n  font-size: 24px;\n  font-weight: 400; }\n\ncanvas {\n  position: fixed;\n  top: 49px; }\n\n#main-controls {\n  position: fixed;\n  bottom: 114px;\n  right: 0;\n  background-color: rgba(51, 23, 13, 0.75);\n  margin: 0;\n  padding: .625em;\n  width: 244px;\n  text-align: right; }\n\n#header {\n  position: fixed;\n  left: 0;\n  right: 0;\n  background-color: rgba(51, 23, 13, 0.75);\n  color: #F9F7ED; }\n\n.big-number {\n  padding: 0 .25em;\n  border-radius: .25em;\n  font-size: 1.25em;\n  vertical-align: middle; }\n\n#total-mobs {\n  color: #009701;\n  background-color: #C4FFCC; }\n\n#total-corpses {\n  color: #2C95C9;\n  background-color: #C5FFFF; }\n\n#number-mobs-to-add,\n#mob-category {\n  margin: 0 .5em 0 0; }\n\nli {\n  list-style-type: none; }\n\ninput,\nselect {\n  cursor: pointer;\n  min-width: 44px;\n  border-radius: .5em;\n  border: solid 1px;\n  background-color: #F9F7ED;\n  color: #33170D;\n  line-height: 29px; }\n\ninput[type=submit] {\n  background-color: #C4FFCC;\n  font-weight: 600;\n  text-transform: uppercase;\n  height: 30px; }\n  input[type=submit]:hover {\n    color: #009701; }\n\n.scrollable-window {\n  background-color: rgba(51, 23, 13, 0.2);\n  margin: 0;\n  padding: .625em;\n  width: 250px;\n  height: 100px;\n  overflow: auto;\n  position: fixed;\n  bottom: 0;\n  right: 0;\n  font-family: verdana, sans-serif;\n  font-size: 11px;\n  line-height: 15px; }\n\n.horizontal {\n  margin: 0;\n  padding: 0; }\n  .horizontal li {\n    float: left;\n    margin-left: .5em;\n    text-align: center;\n    line-height: 3em; }\n", ""]);
+	exports.push([module.id, "body {\n  font-family: 'Handlee', cursive;\n  font-size: 16px;\n  font-weight: 400;\n  line-height: 1.5em;\n  margin: 0;\n  background-color: #F9F7ED;\n  color: #33170D; }\n\ninput,\ntextarea,\nkeygen,\nselect,\nbutton {\n  font-family: 'Handlee', cursive;\n  height: 32px;\n  padding: 0 6px;\n  border: 0; }\n\nh1 {\n  margin: 0 6px 0 0;\n  line-height: 49px;\n  font-size: 24px;\n  font-weight: 400; }\n\ncanvas {\n  position: fixed;\n  top: 49px; }\n\n#main-controls {\n  position: fixed;\n  bottom: 114px;\n  right: 0;\n  background-color: rgba(51, 23, 13, 0.75);\n  margin: 0;\n  padding: .625em;\n  width: 244px;\n  text-align: right; }\n\n#header {\n  position: fixed;\n  left: 0;\n  right: 0;\n  background-color: rgba(51, 23, 13, 0.75);\n  color: #F9F7ED; }\n\n.big-number {\n  padding: 0 .25em;\n  border-radius: .25em;\n  font-size: 1.25em;\n  vertical-align: middle; }\n\n#total-mobs {\n  color: #009701;\n  background-color: #C4FFCC; }\n\n#total-corpses {\n  color: #2C95C9;\n  background-color: #C5FFFF; }\n\n#number-mobs-to-add,\n#mob-category {\n  margin: 0 .5em 0 0; }\n\nli {\n  list-style-type: none; }\n\ninput,\nselect {\n  cursor: pointer;\n  min-width: 44px;\n  border-radius: .5em;\n  border: solid 1px;\n  background-color: #F9F7ED;\n  color: #33170D;\n  line-height: 29px; }\n\ninput[type=submit] {\n  -webkit-appearance: none;\n  background-color: #C4FFCC;\n  font-weight: 600;\n  text-transform: uppercase;\n  height: 30px; }\n  input[type=submit]:hover {\n    color: #009701; }\n\n.scrollable-window {\n  background-color: rgba(51, 23, 13, 0.2);\n  margin: 0;\n  padding: .625em;\n  width: 250px;\n  height: 100px;\n  overflow: auto;\n  position: fixed;\n  bottom: 0;\n  right: 0;\n  font-family: verdana, sans-serif;\n  font-size: 11px;\n  line-height: 15px; }\n\n.horizontal {\n  margin: 0;\n  padding: 0; }\n  .horizontal li {\n    float: left;\n    margin-left: .5em;\n    text-align: center;\n    line-height: 3em; }\n", ""]);
 	
 	// exports
 
 
 /***/ },
-/* 185 */
+/* 184 */
 /***/ function(module, exports) {
 
 	/*
@@ -21664,7 +21620,7 @@
 
 
 /***/ },
-/* 186 */
+/* 185 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
@@ -21916,6 +21872,50 @@
 			URL.revokeObjectURL(oldSrc);
 	}
 
+
+/***/ },
+/* 186 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var animateMobMovement = exports.animateMobMovement = function animateMobMovement(mob) {
+	  // Make position and destination comparable.
+	  var posY = Math.floor(mob.position.y);
+	  var posX = Math.floor(mob.position.x);
+	  var desY = Math.floor(mob.destination.y);
+	  var desX = Math.floor(mob.destination.x);
+	
+	  // Has mob arrived at destination?
+	  if (posY === desY && posX === desX) {
+	    mob.position.y = mob.destination.y;
+	    mob.position.x = mob.destination.x;
+	    mob.position.coordinateY = mob.destination.coordinateY;
+	    mob.position.coordinateX = mob.destination.coordinateX;
+	    mob.arrivedAtDestination = true;
+	
+	    return mob;
+	  }
+	
+	  // Animate movement.
+	  if (posY > desY) {
+	    mob.position.y = mob.position.y - mob.speed;
+	  }
+	  if (posY < desY) {
+	    mob.position.y = mob.position.y + mob.speed;
+	  }
+	  if (posX > desX) {
+	    mob.position.x = mob.position.x - mob.speed;
+	  }
+	  if (posX < desX) {
+	    mob.position.x = mob.position.x + mob.speed;
+	  }
+	
+	  return mob;
+	};
 
 /***/ }
 /******/ ]);
